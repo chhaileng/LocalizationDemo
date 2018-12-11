5bfad3e60b99b7fcdc84608070c7fea0be97048e
Chhaileng Peng <chhailengpeng@gmail.com>
2018-12-11 10:32:15 +0700

    Initial Commit

---
+++ LocalizationDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalizationDemo
+//
+//  Created by Chhaileng Peng on 12/11/18.
+//  Copyright © 2018 Chhaileng Peng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
